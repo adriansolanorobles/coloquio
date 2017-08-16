8bc3953dfa805359df2d935cc0a9e2c8de52dd54
adriansolanorobles <adrian.solano@protonmail.com>
2017-08-15 21:56:04 -0500

    m

--- modules/asistentes/models.py
+++ modules/asistentes/models.py
@@ -11,7 +11,7 @@ class Asistentes(models.Model):
         ('Si', 'Si'),
         ('No', 'No'),
 	)
-	DESEAS_ASISTIR_A_LOS_TALLERES_CHOICES = (
+	DAT_CHOICES = (
         ('Si', 'Si'),
         ('No', 'No'),
 	)
@@ -41,7 +41,7 @@ class Asistentes(models.Model):
 	nombramiento = models.CharField(max_length=255,blank=True,null=True)
 	taller = models.CharField(max_length=255, choices=TALLERES_CHOICES)
 	te_gustaria_recibir_informacion = models.BooleanField()
-	deseas_asistir_a_los_talleres = models.CharField(max_length=2, choices=DESEAS_ASISTIR_A_LOS_TALLERES_CHOICES,blank=True,null=True)
+	deseas_asistir_a_los_talleres = models.CharField(max_length=2,choices=DAT_CHOICES,blank=True,null=True)
 	ceated = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
