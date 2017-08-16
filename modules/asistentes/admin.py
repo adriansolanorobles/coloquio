651d062160b2dcb0a4a0d8b44cdf615e58f6344c
adriansolanorobles <adrian.solano@protonmail.com>
2017-08-16 01:38:02 -0500

    agregue el admin de asistentes

--- modules/asistentes/admin.py
+++ modules/asistentes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Asistentes
+
+class AsistentesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Asistentes, AsistentesAdmin)from django.contrib import admin
 
 # Register your models here.
